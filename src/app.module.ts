bb178a8b5d5c0c9a387e243f9b17149670022afc
MohammedAldalooj <96469073+mohammedAldalooj-penny@users.noreply.github.com>
2023-07-13 12:03:32 +0300

    initial project structure

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [DatabaseModule, UsersModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
