db60030d598182570229b90d4723e3e98e6b2c70
MohammedAldalooj <96469073+mohammedAldalooj-penny@users.noreply.github.com>
2023-07-31 00:20:19 +0300

    remove domain

--- src/user/auth/auth.controller.ts
+++ src/user/auth/auth.controller.ts
@@ -33,7 +33,6 @@ export class AuthController {
         sameSite: 'lax',
         expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // One week from now
         secure: false,
-        domain: 'https://pluginsfactory.netlify.app',
       });
       res.json({
         message: 'User registered successfully',
@@ -79,7 +78,6 @@ export class AuthController {
         sameSite: 'lax',
         expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // One week from now
         secure: true,
-        domain: 'https://pluginsfactory.netlify.app',
       });
       res.json({
         message: 'User logged in successfully',
