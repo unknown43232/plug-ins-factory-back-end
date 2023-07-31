508733f89b6e25144a50b7684879a02b6cf8d9d3
MohammedAldalooj <96469073+mohammedAldalooj-penny@users.noreply.github.com>
2023-07-31 14:04:51 +0300

    remove getting jwt from cookies

--- src/middlewares/jwt-auth.guard.ts
+++ src/middlewares/jwt-auth.guard.ts
@@ -3,10 +3,10 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  getRequest(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
-    request.headers.authorization =
-      request.headers.authorization || `Bearer ${request.cookies['token']}`;
-    return request;
-  }
+  // getRequest(context: ExecutionContext) {
+  //   const request = context.switchToHttp().getRequest();
+  //   request.headers.authorization =
+  //     request.headers.authorization || `Bearer ${request.cookies['token']}`;
+  //   return request;
+  // }
 }
